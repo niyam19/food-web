b42662363337343b47ca2d80a09a73a57b2e43f3
Niyam Singhvi <niyam19@gmail.com>
2025-04-27 17:12:08 +0530

    authentication improved frontend and header addition and ui fixes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'food-web';
+  originalToken: string | null = null;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.originalToken = localStorage.getItem('token');
+  }
+
+  @HostListener('window:storage', ['$event'])
+  onStorageChange(event: StorageEvent) {
+    if (event.key === 'token') {
+      const currentToken = event.newValue;
+      if (!currentToken) {
+        this.router.navigate(['/login']);
+      } else if (currentToken !== this.originalToken) {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+      }
+    }
+  }
+
+  isAuthPage(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl.includes('/login') || currentUrl.includes('/signup');
+  }
 }
--- frontend/src/app/auth-guard/auth.guard.ts
+++ frontend/src/app/auth-guard/auth.guard.ts
@@ -1,13 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const jwtHelper = new JwtHelperService()
   const token = localStorage.getItem('token');
 
-  if(token){
+  try {
+    const isExpired = jwtHelper.isTokenExpired(token);
+    if (isExpired) {
+      localStorage.removeItem('token');
+      router.navigate(['/login']);
+      return false;
+    }
+
     return true;
-  } else {
+  } catch (error) {
+    console.error('Invalid token detected:', error);
+    localStorage.removeItem('token');
     router.navigate(['/login']);
     return false;
   }
